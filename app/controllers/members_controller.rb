453413aff48bec2cd27cb3926df1a0bfa2c00594
Martin Tomes <martin@tomes.org.uk>
2015-01-03 10:58:36 +0000

    Can now edit a member's email address.

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -16,10 +16,37 @@ class MembersController < ApplicationController
     @challenge = Challenge.find(challenge_id)
   end
 
+  # GET /challenges/:challenge_id/members/:id/edit
+  def edit
+    challenge_id = params[:challenge_id]
+    @challenge = Challenge.find(challenge_id)
+    @member = Member.find(params[:id])
+    @post_url = challenge_member_path
+  end
+
+  # PATCH/PUT /challenges/:challenge_id/members/:id
+  # PATCH/PUT /members/1.json
+  def update
+    respond_to do |format|
+      if @member.update(member_params)
+        format.html { redirect_to({ action: :show, challenge_id: params[:challenge_id], id: @member.id}, notice: 'Member was successfully updated.' )}
+        format.json { render :show, status: :ok, location: @member }
+      else
+        format.html { render :edit }
+        format.json { render json: @member.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def set_member
     @member = Member.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def member_params
+    params.require(:member).permit(:email)
   end
 
 end
