499badf83410df9f6b95f29835eec7ed22b5ef0e
Martin Tomes <martin@tomes.org.uk>
2015-01-29 07:16:59 +0000

    Use sendmail to deliver mail on the production system. It avoids certificate errors.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,10 +76,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address:              'localhost',
-      port:                 25,
-      domain:               'tomes.org',
-      enable_starttls_auto: false  }
+  config.action_mailer.delivery_method = :sendmail
 end
